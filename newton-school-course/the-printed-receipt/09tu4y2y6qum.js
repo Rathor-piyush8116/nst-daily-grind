520bdd5b44828a5985f428f64bf920fb35511ebb
Piyush Rathor <piyushrathour1046@gmail.com>
2026-09-19 16:19:18 +0530

    Sync Solution: The Printed Receipt [Score: 100%]
    A shopkeeper wants to generate a simple digital bill for a customer. You are given the customer&#39;s name and the prices of three products purchased by the customer.
    
    Your task is to calculate the total price of the three products and print the bill in the exact format specified.
    
    The customer&#39;s name should be displayed after Customer:. The three product prices should be displayed on the same line, separated by |. Finally, print the sum of all three prices after Total:Rs..
    
    ### Input
    
    * The first line contains the customer's name.
    
    * The second line contains three space-separated integers representing the prices of the three products.
    
    ### Output
    Print the bill in the following format:
    
    Customer:Name
    Prices:Price1|Price2|Price3
    Total:Rs.Total
    
    Replace Name with the customer's name, Price1, Price2, and Price3 with the given prices, and Total with their sum.
    
    ### Example
    Input:
    Adarsh
    120 250 80
    
    Output:
    Customer:Adarsh
    Prices:120|250|80
    Total:Rs.450
    
    Input:
    Rahul
    500 150 350
    
    Output:
    Customer:Rahul
    Prices:500|150|350
    Total:Rs.1000

---
+++ newton-school-course/the-printed-receipt/09tu4y2y6qum.js
@@ -0,0 +1,12 @@
+// ─── 2 ───
+name = input()
+p1, p2, p3= map(int,input().split())
+total = p1+p2+p3
+print("Customer:",name,sep="")
+print("Prices:",p1,"|", p2,"|",p3,sep="")
+print("Total:Rs.",total,sep="")
+
+// ─── 31 ───
+Customer:Adarsh
+Prices:120|250|80
+Total:Rs.450
