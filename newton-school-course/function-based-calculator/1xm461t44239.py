73f1cb344aab58a2aaf7b98088b3721f8e1ddc3d
Piyush Rathor <piyushrathour1046@gmail.com>
2026-09-21 13:06:44 +0530

    Sync Solution: Function Based Calculator. [Score: 100%]
    You are building a calculator &mdash; but this time, you&#39;re not allowed to use direct arithmetic operators.
    
    All the arithmetic operations have already been defined as functions in the code for you.
    
    Your Task:
    
     * Read two integers and one operator symbol from the input
    
     * Based on the operator, call the appropriate function
    
     * Print the result returned by that function
    
     * If the operator is invalid (other than &#39; + &#39; , &#39; - &#39; , &#39; * &#39;, , &#39; // &#39;, &#39; % &#39; ) for this question , print Invalid Operator
    
    Note: Do not use arithmetic operators directly in your code. Use only the pre-defined functions provided as comments in the starter code.
    
    ### Input
    The first line of input contains two space-separated integers, representing A and B.
    The second line contains a string op, representing the operation to perform.
    
    ### Output
    A single line — the result of the operation, or Invalid Operator if the symbol is not supported.
    
    Note: Use pre-defined functions.
    
    ### Example
    Input
    20 10
    +
    Output
    30
    
    Input
    5 3
    ^
    Output
    Invalid Operator

---
+++ newton-school-course/function-based-calculator/1xm461t44239.py
@@ -0,0 +1,40 @@
+// ─── 2 ───
+'''
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a // b
+
+def modulo(a, b):
+    return a % b
+'''
+
+# Your code starts here
+a,b = map(int,input().split())
+op = input()
+if op == "+":
+    print(add(a,b))
+elif op =="-":
+    print(subtract(a,b))
+elif op == "*":
+    print(multiply(a,b))
+elif op == "//":
+    print(divide(a,b))
+elif op == "%":
+    print(modulo(a,b))
+else:
+    print("Invalid Operator")
+
+
+
+
+
+// ─── 9 ───
+30
