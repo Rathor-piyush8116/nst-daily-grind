26c44bf0a6cee41a2baf5df6cfb48752f7bc9ad6
Piyush Rathor <piyushrathour1046@gmail.com>
2026-09-21 18:20:56 +0530

    Sync Solution: Celsius to Fahrenheit Conversion - 2 [Score: 100%]
    You are given an integer, representing the temperature in celsius.
    
    Your task is to convert it to Fahrenheit, using the below given formula:
    
    Fahrenheit = (Celsius &times; 9 / 5) + 32 
    You must complete the function celsius_to_fahrenheit(celsius) that returns the equivalent temperature in Fahrenheit.
    
    Function Signature:
    
    def celsius_to_fahrenheit(celsius): 
    
    It takes a single parameter, celsius, the temperature in Celsius.
    
    ### Input
    User Task
    As this is a functional problem, there is no need to handle input from the user. Your task is to complete the function celsius_to_fahrenheit(celsius), which takes single parameter, celsius, the temperature in celsius.
    
    Custom Input
    The first and only line of input contains a single integer, celsius, representing the temperature in celsius.
    
    ### Output
    Return the corresponding temperature in Fahrenheit (as a float).
    
    ### Example
    Input
    0
    Output
    32.0
    Explanation
    Input Celsius = 0
    Fahrenheit = (0 × 9/5) + 32 = 0 + 32 = 32.0
    So the output is 32.0.
    
    Input
    37
    Output
    98.6
    Explanation
    Input Celsius = 37
    Fahrenheit = (37 × 9/5) + 32 = 98.6
    So the output is 98.6.

--- newton-school-course/celsius-to-fahrenheit-conversion-2/kqvs0ka7nsoe.py
+++ newton-school-course/celsius-to-fahrenheit-conversion-2/kqvs0ka7nsoe.py
@@ -1,8 +1,8 @@
-// ─── 19 ───
+// ─── 27 ───
 def celsius_to_fahrenheit(celsius):
    #Return the corresponding temperature in Fahrenheit (as a float).
    fahrenheit = (celsius*9/5)+32
    return fahrenheit
 
-// ─── 20 ───
+// ─── 28 ───
 32
