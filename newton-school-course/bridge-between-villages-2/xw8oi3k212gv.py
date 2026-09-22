08f097018531baac34c0e06cf54b210d8e544668
Piyush Rathor <piyushrathour1046@gmail.com>
2026-09-22 13:15:34 +0530

    Sync Solution: Bridge Between Villages -2 [Score: 100%]
    In a rural district, three villages &mdash; Raga, Sora, and Tula &mdash; are planning to connect to each other with straight bridges. The government has provided the proposed lengths for these bridges as follows:
    
     
     Raga to Sora: a kilometers
    
     
    
     
     Sora to Tula: b kilometers
    
     
    
     
     Tula to Raga: c kilometers
    
     
    
    You are required to implement the function is_triangle(a, b, c) that returns "YES" or "NO" depending on whether the given bridge lengths can form a valid triangle.
    
    Function Signature:
    
    def is_triangle(a, b, c): which takes three parameters: a: represents proposed length from Raga to Sora, b: represents proposed length from Sora to Tula, c: represents proposed length from Tula to Raga
    
    ### Input
    User Task
    As this is a functional problem, there is no need to handle input from the user. Your task is to complete the function is_triangle(a, b, c), which takes three parameters: a: represents proposed length from Raga to Sora, b: represents proposed length from Sora to Tula, c: represents proposed length from Tula to Raga
    
    Custom Input
    Input consist of single line that contains three space-separated positive integers:
    a b c
    
    ### Output
    Return YES if the construction plan can go ahead, otherwise return NO.
    
    ### Example
    Input:
    6 7 10
    
    Output:
    YES
    
    Explanation
    The bridge lengths are (6, 7, 10) which satisfy
    
    6 + 7 > 10 (13 > 10), 7 + 10 > 6 (17 > 6), and 10 + 6 > 7 (16 > 7).
    
    Since all pairs of bridges can connect to enclose a region, the output is "YES".
    
    Input:
    3 5 8
    
    Output:
    NO
    
    Explanation
    The bridge lengths are(3, 5, 8) not follows the below condition
    
    3 + 5 > 8 → 8 > 8 (False).
    5 + 8 > 3 -> 13 > 3 (True)
    3 + 8 > 5 -> 11 > 5 (True)
    Since one condition is False, bridges cannot enclose a region; the output is "NO".

---
+++ newton-school-course/bridge-between-villages-2/xw8oi3k212gv.py
@@ -0,0 +1,7 @@
+// ─── 2 ───
+def is_triangle(a,b,c):
+    #Return YES if the construction plan can go ahead, otherwise return NO.
+    return "YES" if a + b > c and b + c > a and a + c > b else "NO"
+
+// ─── 3 ───
+6 7 10
