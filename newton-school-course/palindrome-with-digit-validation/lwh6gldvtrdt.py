4059779266f4ac4637d6f2614b1b0c626c9d69ac
Piyush Rathor <piyushrathour1046@gmail.com>
2026-09-23 13:13:50 +0530

    Sync Solution: Palindrome with Digit Validation [Score: 100%]
    Write a program that take an integer number n and an integer digit d, then prints one of these messages based on the conditions below:
    
    1. If n is a palindrome and its last digit is d:
    
     "Number is palindrome and last digit is d"
    
    2. If n is a palindrome but its last digit is not d:
    
     "Number is palindrome and last digit is not d"
    
    3. If n is not a palindrome:
    
     "Number is not a palindrome"
    
    Note:
    
    Use the provided isPalindrome(n) function returns True or False. Do not rewrite it.
    
     
    
    A palindrome number is a number that reads the same backward as forward.
    Examples: 121, 1331, 44444
    
    Numbers that are not palindrome do not read the same backward.
    Examples: 123, 4567, 1201
    
    ### Input
    The Input will be the two space-separated integers n and d.
    
    ### Output
    The output will be the string message as per the problem description.
    
    ### Example
    Input 1:
    121 1
    
    Output 1:
    Number is palindrome and last digit is 1
    
    Explanation:
    121 is palindrome and last digit in 121 is d as well 
    so print "Number is palindrome and last digit is 1"
    
    Input 2:
    121 3
    
    Output 2:
    Number is palindrome and last digit is not 3
    
    Explanation:
    121 is palindrome but last digit in 121 is not d that is 3,
    so print "Number is palindrome and last digit is not 3"
    
    Input 3:
    123 3
    
    Output 3:
    Number is not a palindrome
    
    Explanation:
    123 is not even palindrome so we don't check for last digit
    just print Number is not a palindrome

---
+++ newton-school-course/palindrome-with-digit-validation/lwh6gldvtrdt.py
@@ -0,0 +1,14 @@
+// ─── 2 ───
+# Your code here
+n,d=map(int,input().split())
+a = isPalindrome(n)
+if a==1 and n%10==d :
+    print(f"Number is palindrome and last digit is {d}")
+elif a==1  and n%10!=d :
+    print(f"Number is palindrome and last digit is not {d}")
+else:
+     print("Number is not a palindrome")
+    
+
+// ─── 11 ───
+Number is palindrome and last digit is 1
